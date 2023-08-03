3b34cd5d7b2d1faa78b34c02f66f95e01c1b8d05
HawkingRadiation42 <shrey.209303098@muj.manipal.edu>
2023-08-04 01:49:53 +0530

    debugged the ruby file

--- Formula/nchat.rb
+++ Formula/nchat.rb
@@ -1,34 +1,30 @@
 class Nchat < Formula
-  desc "Terminal-based Telegram / WhatsApp client for Linux and macOS"
+  desc "Terminal-based Telegram / WhatsApp Client for Linux and Macos"
   homepage "https://github.com/d99kris/nchat"
   url "https://github.com/d99kris/nchat/archive/refs/tags/v3.60.tar.gz"
   sha256 "8a382603d5e9ef1942f796995dc4652d9e9665d1dbcf55349461e81ecebf4bca"
   license "MIT"
 
-  depends_on "gperf"
-  depends_on "cmake"
-  depends_on "openssl"
-  depends_on "ncurses"
   depends_on "ccache"
-  depends_on "readline"
-  depends_on "help2man"
-  depends_on "sqlite"
-  depends_on "libmagic"
+  depends_on "cmake" => :build
   depends_on "go"
+  depends_on "gperf"
+  depends_on "help2man"
+  depends_on "libmagic"
+  depends_on "ncurses"
+  depends_on "openssl"
+  depends_on "readline"
+  depends_on "sqlite"
 
-  def install
-    system "mkdir", "-p", "build"
-    cd "build" do
-      system "cmake", ".."
+  def install 
+    mkdir "build" do 
+      system "cmake", "..", *std_cmake_args
       system "make", "-s"
-      bin.install "nchat"
+      system "make", "install"
     end
-    prefix.install "README.md"
-    prefix.install "LICENSE"
   end
 
-  test do
-    assert_predicate bin/"nchat", :executable?
-    assert_match "nchat v3.64", shell_output("#{bin}/nchat --version")
+  test do 
+    system "#{bin}/nchat", "--version"
   end
 end
